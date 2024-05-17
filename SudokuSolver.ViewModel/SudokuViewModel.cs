8ca53e3fa167f9c0efbcb1a4375092e33725a997
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-17 13:30:01 +0200

    Created ViewModel for SudokuBoard

---
+++ SudokuSolver.ViewModel/SudokuViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SudokuSolver.Model;
+using System.ComponentModel;
+
+namespace SudokuSolver.ViewModel
+{
+    public class SudokuViewModel : INotifyPropertyChanged
+    {
+        private readonly SudokuBoard board;
+
+        public byte this[byte i, byte j]
+        {
+            get => board[i, j];
+            set
+            {
+                board[i, j] = value;
+                OnPropertyChanged($"Item[{i},{j}]");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SudokuViewModel()
+        {
+            board = new SudokuBoard();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
