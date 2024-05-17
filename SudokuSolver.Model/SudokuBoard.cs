ee0de8090db941d0fabc52e5e3926fb3345049f1
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-17 13:16:36 +0200

    Created SudokuBoard model

---
+++ SudokuSolver.Model/SudokuBoard.cs
@@ -0,0 +1,18 @@
+﻿namespace SudokuSolver.Model
+{
+    public class SudokuBoard
+    {
+        public byte[,] Board { get; set; }
+
+        public byte this[byte i, byte j]
+        {
+            get => Board[i, j];
+            set => Board[i, j] = value;
+        }
+
+        public SudokuBoard()
+        {
+            Board = new byte[9, 9];
+        }
+    }
+}
