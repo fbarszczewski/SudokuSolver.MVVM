350ad054e1c7f58d98384c7d269e5da25551e7c4
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-18 17:38:59 +0200

    Modified sudokuCell view & added binding to text functionality

--- SudokuSolver.View/CustomControls/SudokuCell.xaml.cs
+++ SudokuSolver.View/CustomControls/SudokuCell.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SudokuSolver.View.CustomControls
 {
@@ -20,6 +8,14 @@ namespace SudokuSolver.View.CustomControls
     /// </summary>
     public partial class SudokuCell : UserControl
     {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(SudokuCell), new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
         public SudokuCell()
         {
             InitializeComponent();
