3dfcb52cd16f3d1fffe61148522acca2bba10a97
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-18 20:14:23 +0200

    rewritten SudokuBoard model

--- SudokuSolver.Model/SudokuBoard.cs
+++ SudokuSolver.Model/SudokuBoard.cs
@@ -4,15 +4,24 @@
     {
         public byte[,] Board { get; set; }
 
-        public byte this[byte i, byte j]
-        {
-            get => Board[i, j];
-            set => Board[i, j] = value;
-        }
-
         public SudokuBoard()
         {
             Board = new byte[9, 9];
+            ClearBoard();
+        }
+
+        /// <summary>
+        /// Clears sudoku board by replacing every value with 0.
+        /// </summary>
+        private void ClearBoard()
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    Board[i, j] = 0;
+                }
+            }
         }
     }
 }
