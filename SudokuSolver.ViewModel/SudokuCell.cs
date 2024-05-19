1506ef686238cfe6b9d96d7602cd149d6064eb7a
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-19 19:32:18 +0200

    Added helper class SudokuCellValue

---
+++ SudokuSolver.ViewModel/SudokuCell.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace SudokuSolver.ViewModel
+{
+    public class SudokuCell : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal byte Value
+        {
+            get => _value;
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged(nameof(Value));
+                }
+            }
+        }
+
+        private byte _value;
+
+        public SudokuCell(byte value)
+        {
+            _value = value;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static implicit operator byte(SudokuCell observableByte)
+        {
+            return observableByte.Value;
+        }
+
+        public static implicit operator SudokuCell(byte value)
+        {
+            return new SudokuCell(value);
+        }
+    }
+}
