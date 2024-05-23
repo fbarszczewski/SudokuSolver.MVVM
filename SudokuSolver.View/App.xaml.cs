b26a95bce62f344d4df59e0c11676de5d056e912
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-23 16:36:38 +0200

    Added fautofac and created IoC container for view

--- SudokuSolver.View/App.xaml.cs
+++ SudokuSolver.View/App.xaml.cs
@@ -1,14 +1,34 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Autofac;
+using SudokuSolver.Model;
+using SudokuSolver.ViewModel;
 
 namespace SudokuSolver.View
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterType<SudokuBoard>().As<ISudokuBoard>();
+			builder.RegisterType<SudokuViewModel>();
+
+			IContainer container = builder.Build();
+
+			SudokuViewModel sudokuViewModel = container.Resolve<SudokuViewModel>();
+
+			var mainWindow = new MainWindow
+			{
+				DataContext=sudokuViewModel
+			};
+			mainWindow.Show();
+
+			base.OnStartup(e);
+		}
+	}
 
 }
