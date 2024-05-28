9c5edba4214c1fbba58c0e98d62192ea07cf93bc
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-28 13:48:40 +0200

    added sudokuDataManager

---
+++ SudokuSolver.Model/Services/SudokuDataManager.cs
@@ -0,0 +1,25 @@
+﻿using SudokuSolver.Model.Interfaces;
+
+namespace SudokuSolver.Model.Services
+{
+	public class SudokuDataManager
+	{
+		private readonly ISudokuParser _parser;
+
+		public SudokuDataManager(ISudokuParser parser)
+		{
+			_parser = parser;
+		}
+
+		public void LoadSudoku(ISudokuFile sudokuFile)
+		{
+			_parser.LoadBoards(sudokuFile);
+		}
+
+		public void SaveSudoku(ISudokuFile sudokuFile)
+		{
+			_parser.SaveBoards(sudokuFile);
+		}
+
+	}
+}
