ca75c2d55d86b7a5c81562e287bf2781754138a4
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-29 16:44:23 +0200

    updated to use SudokuBoard. added implementation

---
+++ SudokuSolver.Model/Models/SudokuFile.cs
@@ -0,0 +1,19 @@
+﻿using SudokuSolver.Model.Interfaces;
+
+namespace SudokuSolver.Model.Models
+{
+	public class SudokuFile : ISudokuFile
+	{
+		public List<SudokuBoard> Boards { get; set; }
+		public string DataPath { get; set; }
+		public string FileType { get; set; }
+		public string? Content { get; set; }
+
+		public SudokuFile(List<SudokuBoard> boards,string dataPath,string fileType)
+		{
+			Boards = boards;
+			DataPath = dataPath;
+			FileType = fileType;
+		}
+	}
+}
