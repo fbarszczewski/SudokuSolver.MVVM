fe6f60e646d166d062d44cf91078bb9b0d4f2b46
Filip Barszczewski <fbarszczewski@outlook.com>
2024-05-30 23:13:11 +0200

    created SudokuSolverManager

--- SudokuSolver.Model/Services/SudokuSolverManager.cs
+++ SudokuSolver.Model/Services/SudokuSolverManager.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SudokuSolver.Model.Interfaces;
+using SudokuSolver.Model.Models;
+using SudokuSolver.Model.SolvingAlgorithms;
 
 namespace SudokuSolver.Model.Services
 {
 	internal class SudokuSolverManager
 	{
+		private readonly List<ISudokuSolver> _solvers;
+
+		public SudokuSolverManager()
+		{
+			_solvers = new List<ISudokuSolver>
+			{
+				new SimpleBacktrackingSolver()
+
+				// add more solvers here
+			};
+		}
+
+		public bool Solve(SudokuBoard board,ref string firstAlgorithm)
+		{
+			if(!SudokuValidator.IsValidBoard(board))
+			{
+				throw new ArgumentException("Invalid Sudoku board");
+			}
+
+			// try to solve the board with the first algorithm
+			foreach(ISudokuSolver solver in _solvers)
+			{
+				if(solver.GetName() == firstAlgorithm)
+				{
+					if(solver.Solve(board))
+					{
+						firstAlgorithm = solver.GetName();
+						return true;
+					}
+					break;
+				}
+			}
+
+			// try to solve the board with the other algorithms
+			foreach(ISudokuSolver solver in _solvers)
+			{
+				if(solver.GetName() != firstAlgorithm)
+				{
+					if(solver.Solve(board))
+					{
+						firstAlgorithm = solver.GetName();
+						return true;
+					}
+					break;
+				}
+			}
+
+			return false;
+		}
 	}
 }
