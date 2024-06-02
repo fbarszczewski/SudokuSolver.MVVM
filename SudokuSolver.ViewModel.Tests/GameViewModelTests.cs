0636bbf674a7673da0c9c93eb5b9115b938c4e17
Filip Barszczewski <fbarszczewski@outlook.com>
2024-06-03 01:36:25 +0200

    modified some test to Theory tests

--- SudokuSolver.ViewModel.Tests/GameViewModelTests.cs
+++ SudokuSolver.ViewModel.Tests/GameViewModelTests.cs
@@ -13,6 +13,20 @@ namespace SudokuSolver.ViewModel.Tests
 		private AppViewModel? _appViewModel;
 		private GameViewModel? _gameViewModel;
 
+		public static IEnumerable<object[]> TestBoard =>
+		new List<object[]>
+		{
+			new object[] { new byte[,] { {2,8,4,6,3,7,1,5,9}, {7,1,5,2,9,8,6,4,3}, {3,6,9,1,4,5,2,7,8}, {2,3,7,1,8,5,9,4,6}, {4,9,1,7,6,2,8,3,5}, {5,1,9,2,6,8,4,3,7}, {3,7,6,4,5,2,1,8,9}, {4,2,8,7,3,1,2,9,6}, {6,1,3,5,9,4,7,2,8} } },
+			new object[] { new byte[,] { {4,7,8,2,6,1,9,5,3}, {1,2,3,4,5,6,7,8,9}, {7,3,1,9,2,6,5,4,8}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {4,9,2,8,5,3,9,1,7}, {1,6,3,7,4,9,6,8,2}, {8,5,9,1,7,2,6,3,4}, {5,6,8,7,1,9,2,8,3} } },
+			new object[] { new byte[,] { {5,3,1,7,4,9,6,2,8}, {2,7,4,6,3,8,1,9,5}, {9,4,5,2,8,1,3,7,6}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,8,7,3,6,5,4,9,2}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {3,2,6,4,7,8,9,5,1} } },
+			new object[] { new byte[,] { {9,4,2,5,7,3,6,1,8}, {8,6,1,9,2,4,3,7,5}, {7,5,3,8,1,6,9,4,2}, {2,3,7,1,8,5,9,4,6}, {6,8,4,3,9,7,5,2,1}, {5,1,9,2,6,8,4,3,7}, {3,7,6,4,5,2,1,8,9}, {4,2,8,7,3,1,2,9,6}, {6,1,3,5,9,4,7,2,8} } },
+			new object[] { new byte[,] { {4,7,8,2,6,1,9,5,3}, {3,9,6,5,4,2,8,1,7}, {7,3,1,9,2,6,5,4,8}, {1,5,2,6,7,9,3,8,4}, {9,4,7,8,3,5,6,2,1}, {4,9,2,8,5,3,9,1,7}, {1,6,3,7,4,9,6,8,2}, {8,5,9,1,7,2,6,3,4}, {5,6,8,7,1,9,2,8,3} } },
+			new object[] { new byte[,] { {6,9,1,3,7,4,2,5,8}, {2,3,7,1,8,5,9,4,6}, {8,5,4,9,2,6,7,3,1}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {2,6,3,9,7,4,8,1,5}, {9,7,8,2,5,1,3,4,7}, {4,2,5,6,3,8,4,9,1} } },
+			new object[] { new byte[,] { {5,4,9,8,6,2,1,7,3}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9}, {1,2,3,4,5,6,7,8,9} } },
+			new object[] { new byte[,] { {4,7,8,2,6,1,9,5,3}, {3,9,6,5,4,2,8,1,7}, {7,3,1,9,2,6,5,4,8}, {1,5,2,6,7,9,3,8,4}, {9,4,7,8,3,5,6,2,1}, {4,9,2,8,5,3,9,1,7}, {1,6,3,7,4,9,6,8,2}, {8,5,9,1,7,2,6,3,4}, {5,6,8,7,1,9,2,8,3} } },
+			new object[] { new byte[,] { {5,3,1,7,4,9,6,2,8}, {2,7,4,6,3,8,1,9,5}, {9,4,5,2,8,1,3,7,6}, {8,1,7,3,5,2,4,6,9}, {6,2,9,4,1,7,8,5,3}, {1,8,7,3,6,5,4,9,2}, {7,6,3,9,2,4,5,1,8}, {1,2,3,4,5,6,7,8,9}, {3,2,6,4,7,8,9,5,1} } },
+			new object[] { new byte[,] { {6,9,1,3,7,4,2,5,8}, {2,3,7,1,8,5,9,4,6}, {8,5,4,9,2,6,7,3,1}, {4,7,6,2,9,1,3,8,5}, {5,1,8,6,4,3,7,9,2}, {3,6,9,5,7,2,8,1,4}, {2,6,3,9,7,4,8,1,5}, {9,7,8,2,5,1,3,4,7}, {4,2,5,6,3,8,4,9,1} } }
+		};
 
 		public GameViewModelTests()
 		{
@@ -77,66 +91,42 @@ namespace SudokuSolver.ViewModel.Tests
 		}
 
 
-		[Fact]
-		public void InitializeCellCollection_WhenCalledWithDifferentBoard_UpdateGameCells()
+		[Theory]
+		[MemberData(nameof(TestBoard))]
+		public void InitializeCellCollection_WhenCalledWithDifferentBoard_UpdateGameCells(byte[,] board)
 		{
 			//Arrange
 			ClearTestEnvironment();
 			SetupTestEnvironment();
-			var customBoard = new byte[9,9]
-			{
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-					{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-			};
 
 			//Act
 			// To call InitializeGameCells() we need to use UpdateGameBoard()
-			_gameViewModel!.UpdateGameBoard(new SudokuBoard(customBoard));
+			_gameViewModel!.UpdateGameBoard(new SudokuBoard(board));
 
 			//Assert
 			Assert.NotNull(_gameViewModel.GameCells);
 			Assert.Equal(81,_gameViewModel.GameCells.Count);
 
-			Assert_GameCells_With_Board(customBoard,_gameViewModel);
+			Assert_GameCells_With_Board(board,_gameViewModel);
 		}
 
 
-		[Fact]
-		public void GameCell_PropertyChanged_UpdatesModelCorrectly()
+		[Theory]
+		[MemberData(nameof(TestBoard))]
+		public void GameCell_PropertyChanged_UpdatesModelCorrectly(byte[,] board)
 		{
 			// Arrange
 			ClearTestEnvironment();
 			SetupTestEnvironment();
 			var k = 0;
-			var customBoard = new byte[9,9]
-			{
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-			};
-			var sudokuBoard = new SudokuBoard(customBoard);
 
 			// Act
-
 
 			for(var i = 0;i < 9;i++)
 			{
 				for(var j = 0;j < 9;j++)
 				{
-					_gameViewModel!.GameCells![k].Value = customBoard[i,j].ToString();
+					_gameViewModel!.GameCells![k].Value = board[i,j].ToString();
 					k++;
 				}
 			}
